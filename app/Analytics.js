0c8f3f6c5c8909ed5bba7663853fde75c1b9c5da
Mariusz Krawczyk <przyslony@gmail.com>
2022-11-30 02:24:04 +0000

    add quokka to project

--- app/Analytics.js
+++ app/Analytics.js
@@ -8,18 +8,6 @@ function AnalyticsWrapper() {
 
   <Analytics />
 
-  <script strategy="lazyOnload" src={'https://www.googletagmanager.com/gtag/js?id=G-DMN03FGPWF'} />
-
-<script strategy="lazyOnload">
-    {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-DMN03FGPWF'}', {
-        page_path: window.location.pathname,
-        });
-    `}
-</script>
   </>
   );
 }
